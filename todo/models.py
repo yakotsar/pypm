c8eb5b0560503a529257b9166582f03e4ba7c2a6
yakotsar <yakotsar@yandex.ru>
2019-05-21 15:42:45 +0300

    todo model added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+        
+    title    = models.CharField(max_length=10)
+    text     = models.TextField()
+
+    def __str__(self):
+        return self.title
